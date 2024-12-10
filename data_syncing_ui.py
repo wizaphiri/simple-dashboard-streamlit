2a61b64376019d8226238e00b7f073e3c6067fc3
Wiza <64467592+wizaphiri@users.noreply.github.com>
2024-12-10 20:21:03 +0200

    Update data_syncing_ui.py

--- data_syncing_ui.py
+++ data_syncing_ui.py
@@ -1,28 +1,62 @@
-import streamlit as st
+import requests
+import subprocess
 import pandas as pd
 import mysql.connector
+import streamlit as st
 import plotly.express as px
+from streamlit_lottie import st_lottie
 
 import warnings
 warnings.filterwarnings('ignore')
 
 db_config1 = {
     'host': 'localhost',
-    'user': 'test',
-    'password': 'test',
-    'database': 'nft'
+    'user': 'wiza',
+    'password': 'wiza',
+    'database': 'db1'
 }
 
 connection = mysql.connector.connect(**db_config1)
-query = 'select * from nft.data_syncing_ui2'
+query = 'select * from nft.data_syncing_ui'
 data = pd.read_sql(query, connection)
 
 logs = pd.DataFrame(data)
-st.title("OpenMRS Data Syncing Monitoring")
+# st.title("OpenMRS Data Syncing Monitoring")
+
+success_count = logs[logs['status'] == 'Success'].shape[0]
+total_count = logs['facility_name'].nunique() 
+success_rate = round((success_count / total_count)*100) if total_count > 0 else 0
+
+# Function to load Lottie animation from URL
+def load_lottieurl(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    return response.json()
+
+
+# Conditional display based on success rate
+if success_rate > 70:
+    # Load and display confetti animation
+    lottie_confetti = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_touohxv0.json")
+    title = f"🎉 Syncing Milestone Reached 🎉"
+    subtitle = f"{success_count} ({success_rate}%) POC facilities are syncing successfully"
+    if lottie_confetti:
+        st_lottie(lottie_confetti, height=100, key="confetti")
+else:
+    # Display default monitoring title
+    st.title("OpenMRS Data Syncing Monitoring")
+
+
+st.markdown(f"""
+    <h1 style='text-align: center;'>{title}</h1>
+    <h3 style='text-align: center; color: gray;'>{subtitle}</h3>
+    """, unsafe_allow_html=True)
 
 logs['date'] = pd.to_datetime(logs['date'])
 most_recent_date = logs['date'].max()
 
+# Get unique districts
 unique_districts = logs['district'].unique()
 unique_districts.sort()
 
@@ -35,7 +69,7 @@ with col_date:
 with col_district:
     selected_district = st.selectbox("Select District:", options=["All"] + list(unique_districts))
 
-# Filter 
+# Filter data based on selected date and district
 filtered_logs = logs[logs["date"] == str(selected_date)]
 if selected_district != "All":
     filtered_logs = filtered_logs[filtered_logs["district"] == selected_district]
@@ -90,4 +124,5 @@ if not unsuccessful_logs.empty:
     st.table(unsuccessful_logs[["district", "facility_name", "ip_address", "std_err"]].reset_index(drop=True))
 else:
     st.write("No unsuccessful synchronizations for the selected date and district.")
+
 
